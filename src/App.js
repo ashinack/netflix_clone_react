dd92222f282f9b1d7e205666b594342d8248c243
ashina <ashina@riota.in>
2024-10-02 12:26:26 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-
-import './App.css';
-import React from 'react';
-import {originals,action,comdey,Horror,Romance} from './urls'
-import Banner from './Components/Banner/Banner';
-import Navbar from './Components/Navbar/Navbar';
-import RowPost from './Components/RowPost/RowPost';
+import "./App.css";
+import React from "react";
+import { originals, action, comdey, Horror, Romance } from "./urls";
+import Banner from "./Components/Banner/Banner";
+import Navbar from "./Components/Navbar/Navbar";
+import RowPost from "./Components/RowPost/RowPost";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-    <Navbar/>
-    <Banner/>
-    <RowPost url={originals} title='Netflix Orginals'/>
-    <RowPost url={action} title='Action' isSmall/>
-    <RowPost url={comdey} title='comdey' isSmall/>
-    <RowPost url={Horror} title='Horror' isSmall/>
-     <RowPost url={Romance} title='Romance' isSmall/>
-    </div>
+    <BrowserRouter basename="https://github.com/ashinack/netflix_clone_react.git">
+      <div className="App">
+        <Navbar />
+        <Banner />
+        <RowPost url={originals} title="Netflix Orginals" />
+        <RowPost url={action} title="Action" isSmall />
+        <RowPost url={comdey} title="comdey" isSmall />
+        <RowPost url={Horror} title="Horror" isSmall />
+        <RowPost url={Romance} title="Romance" isSmall />
+      </div>
+    </BrowserRouter>
   );
 }
 
